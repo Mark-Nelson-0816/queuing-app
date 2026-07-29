264bc831e46b6f64debc1739d970ebfa930548ff
Paulo <buta.pauloreeve082527@gmail.com>
2026-07-29 17:55:00 +0800

    initialize

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='border-b border-(--border) font-bold text-lg flex justify-center'>
+      QUEUEING
+    </div>
+  )
+}
+
+export default App
